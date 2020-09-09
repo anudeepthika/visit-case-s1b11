2e9707360a0b70f5d178429d17baa68aa52d8617
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-09-09 10:18:00 +0530

    Update ReceiverExample.cpp

--- Receiver/ReceiverExample.cpp
+++ Receiver/ReceiverExample.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+#include<string>
 
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	string line = "";
+	while (getline(cin, line))
+	{
+		if (line == "")
+			break;
+		cout << line << endl;
+	}
+  return 0;
+}
