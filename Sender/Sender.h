f0ba894352d4c3d007bfc67e45f0daf6a708d480
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-10 11:43:37 +0530

    Update Sender.h

--- Sender/Sender.h
+++ Sender/Sender.h
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <string>
+#include<sstream>
+#include <algorithm>
 
+class CSVReader
+{
+    std::string fileName;
+    std::string delimeter;
+public:
+    CSVReader(const std::string& filename, const std::string& delim = ",") : fileName(filename), delimeter(delim) { }
+    // Function to fetch data from a CSV File
+    std::vector<std::vector<std::string>> fetchData();
+};
