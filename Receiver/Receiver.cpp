33e6b02e0309979d63c19a75e0f70a0cad05a48b
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-09-10 09:44:58 +0530

    Update Receiver.cpp

--- Receiver/Receiver.cpp
+++ Receiver/Receiver.cpp
@@ -1,21 +1,37 @@
-#include<iostream>
-#include<string>
+#include "ReceiverHeader.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
+void Receiver::storeFootfallData(const string& footfallRecordString)
+{
+	vector<int> footfallRecord;
+	string footfallElement;
+	stringstream footfallStream(footfallRecordString);
+	while (getline(footfallStream, footfallElement, ','))
+	{
+		footfallRecord.push_back(stoi(footfallElement));
+	}
+	cout << "Record Pushed" << endl;
+	footfallData.push_back(footfallRecord);
+}
+
 int main()
 {
-	string line = "";
-	cout<<"In receiver main"<<endl;
-	while (getline(cin, line))
+	Receiver receiverObj;
+	string footfallRecordString;
+	cout << "In receiver main" << endl;
+	getline(cin, footfallRecordString);
+	cout << footfallRecordString << endl;
+	while (getline(cin, footfallRecordString))
 	{
-		if (line == "")
-		{
-			cout<<"Cin input over"<<endl;
-			break;
-		}
-		cout << "Printing sender message in receiver" << endl;
-		cout << line << endl;
+		cout << footfallRecordString << endl;
+		receiverObj.storeFootfallData(footfallRecordString);
 	}
-  return 0;
+
+	return 0;
 }
