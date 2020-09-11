85df5efbca8fa8894d63f4b8ff5efbb647559215
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-11 20:52:12 +0530

    Create SenderTest.cpp

---
+++ Sender/SenderTest.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "catch.hpp"
+#include "SenderHeader.h"
+#include<vector>
+#include <cmath>
+
+TEST_CASE("when footfall data containing invalid data is read by sender then it prints the valid data") 
+{
+    REQUIRE(fetchValidateandPrintFootfallData("test-data/samplevisit1.csv")==true);
+}
+
