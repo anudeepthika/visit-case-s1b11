cb5e4f04cb722564a4a7a26d60b2653faf26a649
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-12 11:41:02 +0530

    Update SenderTest.cpp

--- Sender/SenderTest.cpp
+++ Sender/SenderTest.cpp
@@ -1,4 +1,4 @@
-#define CATCH_CONFIG_RUNNER  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "catch.hpp"
 #include "SenderHeader.h"
