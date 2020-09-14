6c650e9000be7899b4aeb5d9adb1a8201858cd15
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-14 10:38:15 +0530

    Add files via upload

---
+++ Sender/SenderTest.cpp
@@ -0,0 +1,13 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "catch.hpp"
+#include "SenderHeader.h"
+#include<vector>
+#include <cmath>
+
+TEST_CASE("when footfall data containing invalid data is read by sender then it prints the valid data") 
+{
+    auto b = Test::fetchValidateandPrintFootfallData("test-data/visitdata1.csv","OutputSenderTestData/output1.csv");
+    REQUIRE(b==true);
+}
+
