4309237b9bed44dc2769568652a1f07f618c6384
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-14 11:24:33 +0530

    Update SenderTest.cpp

--- Sender/SenderTest.cpp
+++ Sender/SenderTest.cpp
@@ -1,7 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "catch.hpp"
-#include "SenderHeader.h"#include <fstream>
+#include "SenderHeader.h"
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,10 +12,10 @@ TEST_CASE("when footfall data containing invalid data is read by sender then it 
 {
      Test::fetchValidateandPrintFootfallData("test-data/visitdata1.csv");
      std::ifstream fin1("OutputSenderTestData/visitdataouttest1.csv");
-    std::string testDataBuffer((istreambuf_iterator<char>(fin1)), istreambuf_iterator<char>());
+    std::string testDataBuffer((std::istreambuf_iterator<char>(fin1)), std::istreambuf_iterator<char>());
     fin1.close();
     std::ifstream fin2("OutputSenderTestData/visitdataout1.csv");
-    std::string obtainedDataBuffer((istreambuf_iterator<char>(fin2)), istreambuf_iterator<char>());
+    std::string obtainedDataBuffer((std::istreambuf_iterator<char>(fin2)), std::istreambuf_iterator<char>());
     fin2.close();
     REQUIRE(testDataBuffer == obtainedDataBuffer);
     
