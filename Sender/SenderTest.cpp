220facc4cf601a488023a9c185d43f2470a7c9da
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-14 18:56:03 +0530

    Update SenderTest.cpp

--- Sender/SenderTest.cpp
+++ Sender/SenderTest.cpp
@@ -35,11 +35,11 @@ TEST_CASE("when footfall data containing some invalid data is read by sender the
 TEST_CASE("when footfall data containing large number of more invalid data is read by sender then it prints no valid data to console") 
 {
      Test::fetchValidateandPrintFootfallData("test-data/invalidvisitdata1.csv");
-     std::ifstream fin1("OutputSenderTestData/nodata.txt");
+     /*std::ifstream fin1("OutputSenderTestData/nodata.txt");
     std::string testData((std::istreambuf_iterator<char>(fin1)), std::istreambuf_iterator<char>());
-    fin1.close();
+    fin1.close();*/
     std::ifstream fin2("OutputSenderTestData/visitdataout1.txt");
     std::string obtainedData((std::istreambuf_iterator<char>(fin2)), std::istreambuf_iterator<char>());
     fin2.close();
-    REQUIRE(testData == obtainedData);    
+    REQUIRE(obtainedData.empty()==true);    
 }
