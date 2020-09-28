f27ed8f5a5518a1ab3ac31ffe70ae879b9d5020e
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-28 15:02:58 +0530

    Update Sender.cpp

--- Sender/Sender.cpp
+++ Sender/Sender.cpp
@@ -215,3 +215,14 @@ void Sender::fetchValidateandPrintFootfallData(std::string filename)
    }
 		
 }
+
+#ifndef TEST
+int main()
+{
+    	std::string filename = "test-data/visitdata2.csv";
+	std::string manuallog = "test-data/manuallog2.csv";
+	Sender::fetchValidateReconcileandPrintFootfallData(filename,manuallog);
+    return 0;
+}
+
+#endif
